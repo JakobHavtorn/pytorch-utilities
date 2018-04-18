450d67db7e6ad5c908607d7cf9ba78f1bf6b7b11
Jakob Havtorn <jakob.havtorn@gmail.com>
2018-04-18 10:45:24 +0200

    Added prints and docstrings

--- reshape.py
+++ reshape.py
@@ -2,9 +2,33 @@ import torch.nn as nn
 
 
 class Flatten(nn.Module):
+    """Flattens the batch.
+    """
     def __init__(self):
         super(Flatten, self).__init__()
 
     def forward(self, x):
         x = x.view(x.size(0), -1)
+        return x
+
+
+class PrintBatch(nn.Module):
+    """Prints the forward propagated batch.
+    """
+    def __init__(self):
+        super(PrintBatch, self).__init__()
+
+    def forward(self, x):
+        print(self.__class__, x)
+        return x
+    
+
+class PrintDim(nn.Module):
+    """Prints the dimension of the forward propagated batch.
+    """    
+    def __init__(self):
+        super(PrintDim, self).__init__()
+
+    def forward(self, x):
+        print(x.size())
         return x
