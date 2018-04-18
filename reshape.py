376825df37845b303883f0a8f206ee0984369932
Jakob Havtorn <jakob.havtorn@gmail.com>
2018-04-18 10:30:48 +0200

    Added flatten layer

---
+++ reshape.py
@@ -0,0 +1,10 @@
+import torch.nn
+
+
+class Flatten(nn.Module):
+    def __init__(self):
+        super(Flatten, self).__init__()
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        return x
